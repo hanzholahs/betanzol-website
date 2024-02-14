af9cee8486ccd417844507ddf3c35783a591ac67
hanzholahs <hanzholahs@gmail.com>
2024-02-14 11:11:46 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,26 @@ module.exports = {
       fontFamily: {
         cursive: ["Pinyon Script", "Dancing Script"],
         kufi: ["Reem Kufi", "sans-serif"]
+      },
+      colors: {
+        primary: '#1f1400',
+        secondary: '#c37f00',
+        light: '#fffaf2',
+        'primary-lighter': '#7f5300',
+        back: '#fff8be'
+      },
+      animation: {
+        'wiggle': 'wiggle 1s ease-in-out infinite'
+      },
+      keyframes: {
+          'wiggle': {
+              '0%, 100%': {
+                  transform: 'rotate(-1deg)'
+              },
+              '50%': {
+                  transform: 'rotate(1deg)'
+              },
+          },
       }
     },
   },
